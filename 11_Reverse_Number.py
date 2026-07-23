c8ea9381179e7a9c3a7684a32901840ddde6485c
Bhushan Kancherla <19310733+bhushan62@users.noreply.github.com>
2026-07-23 13:15:45 +0530

    Reverse Number

---
+++ 11_Reverse_Number.py
@@ -0,0 +1,18 @@
+
+number = int(input("Enter Number"))
+
+reverse = 0
+
+while number > 0:
+
+    digit = number % 10
+
+    reverse = reverse * 10 + digit
+
+
+    number = number // 10
+
+print("Digit:", digit)
+print("Reverse:", reverse)
+print("Remaining:", number)
+print("----------------")
