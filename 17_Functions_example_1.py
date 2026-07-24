42c4d00d457ad3ec17690d36417d32fbbf8d69e4
Bhushan Kancherla <19310733+bhushan62@users.noreply.github.com>
2026-07-24 15:59:16 +0530

    Funtions

---
+++ 17_Functions_example_1.py
@@ -0,0 +1,23 @@
+def welcome():
+    print("Welcome to KLYN")
+
+welcome()
+welcome()
+welcome()
+welcome()
+welcome()
+
+
+def stars():
+    print("***************")
+
+stars()
+print("KLYN Laundry")
+stars()
+
+def welcome(name):
+    print("Welcome", name)
+
+welcome("Bhushan")
+welcome("Customer")
+welcome("KLYN")
